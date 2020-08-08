2b7bec8c8ef30d2758b1cd3d3c4c8e856917ed20
Leon Thomm <leon.thomm@gmx.de>
2020-08-08 09:41:00 +0200

    changed docstrings in env import files

--- Ryven/NIWENV.py
+++ Ryven/NIWENV.py
@@ -1,5 +1,4 @@
 """This file automatically imports all requirements for custom NI widgets, so that they only need to import
-this file.
-This file should lie in the same location as Ryven.py in order to be able to get imported directly."""
+this file. his file should lie in the same location as Ryven.py in order to be able to get imported directly."""
 
 from custom_src.retain import M
