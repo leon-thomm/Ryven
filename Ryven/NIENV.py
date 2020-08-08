a1b453e65a2fd8eb153a247cf99694ca38192bd3
Leon Thomm <leon.thomm@gmx.de>
2020-08-08 09:37:50 +0200

    added docstrings to the new env import files

--- Ryven/NIENV.py
+++ Ryven/NIENV.py
@@ -1,5 +1,6 @@
 """This file automatically imports all requirements for custom NodeInstances, so that they only need to import this
-file."""
+file.
+This file should lie in the same location as Ryven.py in order to be able to get imported directly."""
 
 from custom_src.NodeInstance import NodeInstance
 from custom_src.Node import Node
--- Ryven/NIWENV.py
+++ Ryven/NIWENV.py
@@ -1,4 +1,5 @@
 """This file automatically imports all requirements for custom NI widgets, so that they only need to import
-this file."""
+this file.
+This file should lie in the same location as Ryven.py in order to be able to get imported directly."""
 
 from custom_src.retain import M
