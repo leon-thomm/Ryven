d19f088d67d895fd57f7eda308fa16965d3520ba
leon-thomm <l.thomm@mailbox.org>
2023-05-18 09:34:06 +0200

    unix shell script to recreate UIC files
    now also replaces PySideX imports by qtpy

--- recreate_uic_files.sh
+++ recreate_uic_files.sh
@@ -1,2 +1,19 @@
-pyside2-uic ryven/gui/uic/main_window.ui > ryven/gui/uic/ui_main_window.py
-pyside2-uic ryven/gui/uic/flow.ui > ryven/gui/uic/ui_flow.py
+# for all *.ui files in ryven/gui/uic
+for file in ryven/gui/uic/*.ui; do
+    # get filename without extension
+    filename=$(basename -- "$file")
+    filename="${filename%.*}"
+
+    echo "Processing $file to ui_$filename.py"
+
+    # if an argument --pyside6 is given, use pyside6-uic instead of pyside2-uic
+    if [ "$1" = "--pyside6" ]; then
+        pyside6-uic "$file" > "ryven/gui/uic/ui_$filename.py"
+    else
+        pyside2-uic "$file" > "ryven/gui/uic/ui_$filename.py"
+    fi
+
+    # replace pyside imports with qtpy
+    sed -i 's/from PySide6/from qtpy/g' "ryven/gui/uic/ui_$filename.py"
+    sed -i 's/from PySide2/from qtpy/g' "ryven/gui/uic/ui_$filename.py"
+done
