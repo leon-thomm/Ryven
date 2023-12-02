a5dd3ed4d051cff4738c824ed2fc15c64f4ef718
leon-thomm <l.thomm@mailbox.org>
2023-12-02 23:18:22 +0100

    bugfix from previous commit

--- ryvencore-qt/ryvencore_qt/src/flows/FlowView.py
+++ ryvencore-qt/ryvencore_qt/src/flows/FlowView.py
@@ -937,7 +937,6 @@ class FlowView(GUIBase, QGraphicsView):
         else:  # create new item
             item = NodeItem(
                 node=node,
-                node_gui=(
                 node_gui=
                     (node.GUI if hasattr(node, 'GUI') else NodeGUI)     # use custom GUI class if available
                     ((node, self.session_gui)),                         # calls __init__ of NodeGUI class with tuple arg
